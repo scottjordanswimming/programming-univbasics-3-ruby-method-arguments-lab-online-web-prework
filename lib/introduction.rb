000784d8b90546b060ad59d5bfcbfc1e5c016c91
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-01 16:30:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,6 +8,9 @@ def introduction_with_language(name, language)
 end
   
 def introduction_with_language_optional()
+  puts "Hi, my name is #{name} and I am learning 
+  to program in #{language}."
+end
   
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
