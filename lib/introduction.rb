a1f9524017a6fcb9bd91ced154f86e4dd9e6ea9a
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-01 16:26:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-def introduction(name)
+def #introduction(name)
   puts "Hi, my name is #{name}."
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
