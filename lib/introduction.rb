9787f28e85a5fdba224bfd2d963054dd9d3a4cfe
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-01 16:32:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,8 @@ def introduction_with_language(name, language)
   to program in #{language}."
 end
   
-def introduction_with_language_optional(name, language = Ruby)
+def introduction_with_language_optional(name, language =
+"Ruby")
   puts "Hi, my name is #{name} and I am learning 
   to program in #{language}."
 end
