293f64a8b35144e700ce7044b49e032c1b35118e
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-01 16:24:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,5 @@
-def introduction
+def introduction(name)
+  puts 
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
