11808270430bfc60f88c0c2f2744cacd419b5ac0
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-01 16:27:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,9 @@
-def #introduction(name)
+def introduction(name)
   puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
